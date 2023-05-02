00b26e5e2ef3460e308b696e2bbaf3beae72af75
jdkohls@ucsc.edu <jdkohls@ucsc.edu>
2023-05-02 03:08:14 -0400

    bleh

--- fuzzing/fuzz.go
+++ fuzzing/fuzz.go
@@ -5,16 +5,22 @@ import (
 	"github.com/theyahya/enola"
 )
 
+type responseMsg enola.Result
+
 func Fuzz(usr []byte) int{
 	ctx := context.Background()
 	sh, err := enola.New(ctx)
+
 	if err != nil {
 		panic(err)
+		return 1
 	}
 
-	_, err = sh.SetSite("").Check(string(usr))
+	_, err = sh.SetSite("Crevado").Check(string(usr))
+
 	if err != nil {
 		panic(err)
+		return 1
 	}
 	return 0
 }
